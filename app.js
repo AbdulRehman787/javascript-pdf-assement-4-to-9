70a5db411ab1360681c1f8a907a992244cb39e13
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-23 06:39:19 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,111 +1,85 @@
-// task chapter 4  part 1
+// // pdf chapter no 5
 
-// var a = "Abdul "
-// var b = "Rehman "
-// var c = "Muhammad  Aslam"
-// alert(a+b+c)
-
-// task chapter 4  part 2
-// this is legalvariable
-// var game= "Game";
-// alert("Game")
-
-
-
-// task chapter 4  part 3
-// this is legalvariable
-// var $game= "Come On play the pubg";
-// alert($game)
-
-
-
-// task chapter 4  part 4
-// this is        illegalvariable
-// var *game= "Game";
-// alert(*game)
-
-// task chapter 4  part 5
-// this is illegalvariable
-// var #game= "Game";
-// alert(#Game)
-
-
-
-
-// Start chapter no 5 pdf 
-
-// chapter no 5 part  1
-// var a = 5;
-// var b = 3;
+// var a = 5 ;
+// var b = 5 ;
 // var c = a+b;
-// document.write('SUM OF 3 +5  is equal to ',c)
+// document.write("5+5 is equal to =",c)
 
-
-
-// chapter no 5 part  2
-// var a = 5;
-// var b = 3;
+// pdf chapter no 5 task 2
+// var a = 5 ;
+// var b = 5 ;
 // var c = a-b;
-// document.write('SUM OF  5-3  is equal to ',c)
+// document.write("5-5 is equal to =",c)
 
+// pdf chapter no 5 task 3
 
-
-// chapter no 5 part  3
-// var a = 5;
-// var b = 3;
+// var a = 5 ;
+// var b = 5 ;
 // var c = a*b;
-// document.write('SUM OF  5*3  is equal to ',c)
+// document.write("5*5 is equal to =",c)
 
 
 
-// chapter no 5 part  4
-// var a = 10;
-// var b = 5;
+// pdf chapter no 5 task 4
+
+// var a = 5 ;
+// var b = 5 ;
 // var c = a/b;
-// document.write('SUM OF  10/5 is equal to ',c)
+// document.write("5/5 is equal to =",c)
 
 
-// chapter no 5 part  5
+// var a =5;
+// var b = a - ++a + ++a - --a ;
+//         // 5 -  6  +  7  - 6
+// alert(b)
+
+// var a = 600;
+// var b = 5;
+// var c = a*b;
+// document.write("Total Cost to buy ticket 5 to a movie is", c,"PKR")
 
 
-// var tictet= 3000;
-// var member = 5;
-// var finalAmount = (ticket * memer);
-// alert("finalAmount")
-
-// document.write( "<h1>Table of 4</h1>");
+// document.write("<h1>Table of 4</h1>")
 // document.write("4 * 1 = 4 <br>")
-// document.write("4 * 2 = 8 <br>")
-// document.write("4 * 3 = 12 <br>")
+// document.write("4 * 2 = 8  <br>")
+// document.write("4 * 3 = 12 <br> ")
 // document.write("4 * 4 = 16 <br>")
-// document.write("4 * 5 = 20 <br>")
+// document.write("4 * 5 = 20 <br> ")
 // document.write("4 * 6 = 24  <br>")
-// document.write("4 * 7 = 28  <br>")
-// document.write("4 * 8 = 32 <br>")
-// document.write("4 * 9 = 36  <br>")
-// document.write("4 * 10 = 40 <br> ")
+// document.write("4 * 7 = 28 <br>")
+// document.write("4 * 8 = 32<br> ")
+// document.write("4 * 9 = 36<br> ")
+// document.write("4 * 10 = 40 <br>")
 
 
-// document.write("<h1>Shopping Cart</h1>");
 // var priceofItem1 = 650;
-// var quantityofItem1 =3;
+// var quantityofItem1 = priceofItem1 * 3;
+
 // var priceofItem2 = 100;
-// var quantityofItem2 =7;
-// var shippingCharges = 100;
-// console.log(priceofItem1 * quantityofItem1 + priceofItem2 * quantityofItem2 + shippingCharges);
+// var quantityofItem2 = priceofItem2 * 7;
+// var charges = 100;
+// var totalPrice = quantityofItem1 + quantityofItem2 +100;
+// document.write("Total cost of your order is",totalPrice)
 
 
-// var totalMark =980;
-// var obtMark =804;
-// var percentage =(totalMark - obtMark / 100);
+// 1950+700+100
+
+
+// document.write("MarkSheet");
+// var totMark = 980;
+// var obtMark = 804;
+// var percentage = (totMark - obtMark * 100)
 // console.log(percentage)
 
-// var usDollar = 104.80;
-// var ustotalPrice = usDollar*10;
-// var sudiRiyal = 28;
-// var totalsudiRiyal = sudiRiyal*25;
-// var totalPrice = (ustotalPrice + totalsudiRiyal)
-// console.log(totalPrice)
+// document.write("<h1>Currency in PKR</h1>");
+// var dollar = 104.80;
+// var finalDollar = dollar * 10;
+// var riyal = 28;
+// var finalRiyal = riyal * 25;
+// var pkr = (finalDollar +finalRiyal) 
+// document.write("Total currency in PKR is ",pkr)
+
+
 
 // var a = 5;
 // var b= a+5;
@@ -113,3 +87,48 @@
 // var d = c / 2;
 // alert(d)
 
+// var currentYear = 2021;
+// var birthYear = 2003;
+// var age  = (currentYear-birthYear)
+// document.write("Current Year : 2021 <br> BirthYear = 2003 <br> Age :" ,age )
+
+
+
+// var a = 10;
+// var b = --a + ++a + a++ - --a - --a;
+
+        
+// console.log(b)
+
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+// console.log(b)
+
+// var a = prompt("Enter your name");
+// console.log(a)
+
+// var b = prompt("Enter The those  number you want table");
+// for(i=0;i<=10;i++){
+//     document.write(b + " * " + i + " = " + b*i);
+//     document.write("</br>");
+// }
+
+// var number = prompt("Enter The those  number you want table")
+// for(i=0;i<=10;i++){
+// document.write(number + " *" + i + "="+number*i)
+// document.write("</br>")
+// }
+
+// chapter no 5(Q No 10)
+// var num=prompt("Enter the those number you want table <br> ex 5");
+// var result=(((num+5)*10)/2);
+// document.write("result is "+result);
+// document.write("</br>");
+var total_number = 100;
+var subject1 = prompt("Enter The english numer ")
+var subject2 = prompt("Enter The math numer ")
+var subject3 = prompt("Enter The urdu numer ")
+var obt_mark = subject1 +subject2+  subject3
+alert(obt_mark)
