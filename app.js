18fb38726c66dc7858d7b8ee962a24f5bce43ba5
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-23 06:37:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,115 @@
+// task chapter 4  part 1
+
+// var a = "Abdul "
+// var b = "Rehman "
+// var c = "Muhammad  Aslam"
+// alert(a+b+c)
+
+// task chapter 4  part 2
+// this is legalvariable
+// var game= "Game";
+// alert("Game")
+
+
+
+// task chapter 4  part 3
+// this is legalvariable
+// var $game= "Come On play the pubg";
+// alert($game)
+
+
+
+// task chapter 4  part 4
+// this is        illegalvariable
+// var *game= "Game";
+// alert(*game)
+
+// task chapter 4  part 5
+// this is illegalvariable
+// var #game= "Game";
+// alert(#Game)
+
+
+
+
+// Start chapter no 5 pdf 
+
+// chapter no 5 part  1
+// var a = 5;
+// var b = 3;
+// var c = a+b;
+// document.write('SUM OF 3 +5  is equal to ',c)
+
+
+
+// chapter no 5 part  2
+// var a = 5;
+// var b = 3;
+// var c = a-b;
+// document.write('SUM OF  5-3  is equal to ',c)
+
+
+
+// chapter no 5 part  3
+// var a = 5;
+// var b = 3;
+// var c = a*b;
+// document.write('SUM OF  5*3  is equal to ',c)
+
+
+
+// chapter no 5 part  4
+// var a = 10;
+// var b = 5;
+// var c = a/b;
+// document.write('SUM OF  10/5 is equal to ',c)
+
+
+// chapter no 5 part  5
+
+
+// var tictet= 3000;
+// var member = 5;
+// var finalAmount = (ticket * memer);
+// alert("finalAmount")
+
+// document.write( "<h1>Table of 4</h1>");
+// document.write("4 * 1 = 4 <br>")
+// document.write("4 * 2 = 8 <br>")
+// document.write("4 * 3 = 12 <br>")
+// document.write("4 * 4 = 16 <br>")
+// document.write("4 * 5 = 20 <br>")
+// document.write("4 * 6 = 24  <br>")
+// document.write("4 * 7 = 28  <br>")
+// document.write("4 * 8 = 32 <br>")
+// document.write("4 * 9 = 36  <br>")
+// document.write("4 * 10 = 40 <br> ")
+
+
+// document.write("<h1>Shopping Cart</h1>");
+// var priceofItem1 = 650;
+// var quantityofItem1 =3;
+// var priceofItem2 = 100;
+// var quantityofItem2 =7;
+// var shippingCharges = 100;
+// console.log(priceofItem1 * quantityofItem1 + priceofItem2 * quantityofItem2 + shippingCharges);
+
+
+// var totalMark =980;
+// var obtMark =804;
+// var percentage =(totalMark - obtMark / 100);
+// console.log(percentage)
+
+// var usDollar = 104.80;
+// var ustotalPrice = usDollar*10;
+// var sudiRiyal = 28;
+// var totalsudiRiyal = sudiRiyal*25;
+// var totalPrice = (ustotalPrice + totalsudiRiyal)
+// console.log(totalPrice)
+
+// var a = 5;
+// var b= a+5;
+// var c =  b * 10;
+// var d = c / 2;
+// alert(d)
+
